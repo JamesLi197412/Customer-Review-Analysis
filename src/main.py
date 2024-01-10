bd1891d3429aa3eb0454e6cc80129b8e62c7438a
James Li <zhiyueli.education@gmail.com>
2024-01-10 10:06:21 +0800

    Update files

---
+++ src/main.py
@@ -0,0 +1,20 @@
+
+import numpy as np
+import pandas as pd
+import os
+import time
+
+
+def data_load(path):
+
+    print(f'Loading the dataset')
+
+
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+
+
+
