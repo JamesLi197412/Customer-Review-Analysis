3675abb2fd7d7d1317edf3bf4a919c7742a4e665
James Li <zhiyueli.education@gmail.com>
2024-01-21 23:13:54 +0800

    Export File to OneDrive or Sharepoint

--- src/description.py
+++ src/description.py
@@ -1,44 +1,40 @@
-# Acquire basic information about each dataset
-def missing_columns(df):
-    missing_values = df.isnull().sum()
-    missing_values = missing_values.sort_values(ascending=False)
+import pandas as pd
 
-    return missing_values
+def EDA():
+    print('Loading the data and Analysis its structure')
+    # Load the file with relative path
+    df = pd.read_excel( 'data/CUSTOMER_FEEDBACK.xlsx',sheet_name= 'Sheet')
 
+    features_dtypes = df.dtypes
+    rows, columns = df.shape
 
-def df_info_(dataframe):
-    """
-        Information about the DataFrame
-        # Columns Data Type
-        # Data Frame shape
-        # Columns Name
-        # Columns Description
-    """
-    features_dtypes = dataframe.dtypes
-    rows, columns = dataframe.shape
+    # Missing values
+    missing_values_col = df.isnull().sum()
+    missing_values_sort = missing_values_col.sort_values(ascending=False)
 
-    missing_col = missing_columns(dataframe)
-    features_names = missing_col.index.values
-    missing_values = missing_col.values
+    features_names = missing_values_sort.index.values
+    missing_values = missing_values_sort.values
 
-    print('=' * 50)
+    print('=' * 100)
     print('===> This data frame contains {} rows and {} columns'.format(rows, columns))
-    print('=' * 50)
+    print('=' * 100)
 
     print("{:13}{:13}{:30}{:15}".format('Feature Name'.upper(),
                                         'Data Format'.upper(),
-                                        'Number of Missing Values'.upper(),
+                                        'Number of Missing Values By Columns'.upper(),
                                         'The first few samples'.upper()))
 
     for features_names, features_dtypes, missing_values in zip(features_names, features_dtypes[features_names],
-                                                               missing_values):
+                                                               missing_values_sort):
         print('{:15} {:14} {:20}'.format(features_names, str(features_dtypes), str(missing_values) + '-' +
-                                         str(round(100 * missing_values / sum(missing_col), 3)) + ' %'), end=" ")
+                                         str(round(100 * missing_values / sum(missing_values), 3)) + ' %'), end=" ")
 
         for i in range(5):
-            print(dataframe[features_names].iloc[i], end=",")
+            print(df[features_names].iloc[i], end=",")
 
         print("=" * 50)
 
-def EDA():
-    print('I am thinking')
+    return df
+
+def visulaisation(df):
+    print('Visualisation')
--- src/main.py
+++ src/main.py
@@ -1,22 +1,16 @@
 
 import numpy as np
 import pandas as pd
-from description import df_info_
+from description import EDA
 import openpyxl
 import os
 import time
 
 
-def data_load():
-    df = pd.read_excel( 'data/CUSTOMER_FEEDBACK.xlsx',sheet_name= 'Sheet')
-    print(f'Loading the dataset')
-    return df
-
-
 
 if __name__ == '__main__':
-    customer_feedback = data_load()
-    print(df_info_(customer_feedback))
+
+    print(EDA())
     #start_time = time.time()
 
 
